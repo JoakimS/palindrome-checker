fc846dd42ae205500b94db58ba501f52a5912719
JoakimS <gray775@yahoo.com>
2024-02-16 21:26:29 -0800

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState } from "react";
 
-function App() {
+export default function App() {
+  const [word, setWord] = useState("");
+
+  const isPalindrome = useMemo(() => {
+    return word === word.split("").reverse().join("");
+  }, [word]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <div>
+          <label>word to check</label>
+          <input value={word} onChange={(e) => setWord(e.target.value)} />
+        </div>
+      </form>
+      <div>Is Palindrome:{isPalindrome ? "Yes" : "No"}</div>
     </div>
   );
 }
-
-export default App;
