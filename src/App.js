fb73e00602aaa8fc56d0f5ddf25369336f265e5a
JoakimS <gray775@yahoo.com>
2024-02-17 07:46:29 -0800

    second

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
     <div className="App">
       <form>
         <div>
-          <label>word to check</label>
+          <label>Hello word to check</label>
           <input value={word} onChange={(e) => setWord(e.target.value)} />
         </div>
       </form>
